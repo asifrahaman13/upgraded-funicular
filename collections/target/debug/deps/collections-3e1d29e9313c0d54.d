d25d403320cdd78832732d9bf6984cadede24b09
Asif Rahaman <asifrahaman162@gmail.com>
2024-09-13 18:41:11 +0530

    wip: options

--- collections/target/debug/deps/collections-3e1d29e9313c0d54.d
+++ collections/target/debug/deps/collections-3e1d29e9313c0d54.d
@@ -1,5 +1,5 @@
-/home/asifr/dev/projects/language/rust/collections/target/debug/deps/libcollections-3e1d29e9313c0d54.rmeta: src/main.rs
+/home/asifr/code/developments/language/rust/collections/target/debug/deps/libcollections-3e1d29e9313c0d54.rmeta: src/main.rs
 
-/home/asifr/dev/projects/language/rust/collections/target/debug/deps/collections-3e1d29e9313c0d54.d: src/main.rs
+/home/asifr/code/developments/language/rust/collections/target/debug/deps/collections-3e1d29e9313c0d54.d: src/main.rs
 
 src/main.rs:
